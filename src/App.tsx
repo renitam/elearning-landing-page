6379a9748a1bd15479c97e5ffa7c1025d2e9ab84
renitam <renita.mwangachuchu@gmail.com>
2024-04-03 23:10:26 -0400

    Add index.html starter code to react app and define button style

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from './styles/Shared';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        Get Started
+        <Button>Get Started</Button>
 
-        Maximize skill, minimize budget
+        <h1>Maximize skill, minimize budget</h1>
 
         Our modern courses across a range of in-demand skills will give you the 
         knowledge you need to live the life you want.
